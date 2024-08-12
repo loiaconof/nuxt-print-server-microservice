6d474e5a66057a85164bf9248e3f6912f07aeebb
loiaconof <francesco.loiacono@linksmt.it>
2024-08-12 10:47:13 +0200

    refactor export pdf utils

--- server/utils/export-pdf.ts
+++ server/utils/export-pdf.ts
@@ -3,21 +3,19 @@ import puppeteer from 'puppeteer';
 export async function exportPdf(filename: string, path: string) {
   const html = await $fetch<string>(path);
 
-  const browser = await puppeteer.launch();
-  
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
 
-  await page.setContent(html, { waitUntil: 'networkidle0' });
+  await page.setContent(html, {
+    waitUntil: 'domcontentloaded'
+  });
 
   const pdfBuffer = await page.pdf({
     format: 'A4',
-    printBackground: true,
-    margin: {
-      top: '1in',
-      bottom: '1in',
-      left: '1in',
-      right: '1in'
-    }
+    printBackground: true
   });
 
   await browser.close();
