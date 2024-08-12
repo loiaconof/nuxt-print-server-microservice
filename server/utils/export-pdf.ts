314d24c3340314d4500e2fa18dc979c3610ecee8
loiaconof <francesco.loiacono@linksmt.it>
2024-08-12 13:00:00 +0200

    update exort-pdf to merge default pdf options to option argument

--- server/utils/export-pdf.ts
+++ server/utils/export-pdf.ts
@@ -1,6 +1,7 @@
-import puppeteer from 'puppeteer';
+import puppeteer, { type PDFOptions } from 'puppeteer';
+import { defu } from 'defu';
 
-export async function exportPdf(event: H3Event<EventHandlerRequest>, filename: string, path: string) {
+export async function exportPdf(event: H3Event<EventHandlerRequest>, filename: string, path: string, options?: Partial<{ pdfOptions: PDFOptions }>) {
   const html = await $fetch<string>(path);
 
   const browser = await puppeteer.launch({
@@ -13,10 +14,11 @@ export async function exportPdf(event: H3Event<EventHandlerRequest>, filename: s
     waitUntil: 'domcontentloaded'
   });
 
-  const pdfBuffer = await page.pdf({
+  const defaultOptions: PDFOptions = {
     format: 'A4',
     printBackground: true
-  });
+  };
+  const pdfBuffer = await page.pdf(defu(options?.pdfOptions ?? {}, defaultOptions));
 
   await browser.close();
 
