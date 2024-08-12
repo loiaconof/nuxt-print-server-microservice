e0af1ae2ed41b21d869d1c64c59c520a962b29a8
loiaconof <francesco.loiacono@linksmt.it>
2024-08-12 13:05:16 +0200

    update exort-pdf to merge default puppeteer laungh option  to option argument

--- server/utils/export-pdf.ts
+++ server/utils/export-pdf.ts
@@ -1,24 +1,30 @@
-import puppeteer, { type PDFOptions } from 'puppeteer';
+import puppeteer, { type PuppeteerLaunchOptions, type PDFOptions } from 'puppeteer';
 import { defu } from 'defu';
 
-export async function exportPdf(event: H3Event<EventHandlerRequest>, filename: string, path: string, options?: Partial<{ pdfOptions: PDFOptions }>) {
+export interface ExportPdfOptions { 
+  puppeteerLaunchOpions: PuppeteerLaunchOptions, 
+  pdfOptions: PDFOptions,
+}
+
+export async function exportPdf(event: H3Event<EventHandlerRequest>, filename: string, path: string, options?: Partial<ExportPdfOptions>) {
   const html = await $fetch<string>(path);
 
-  const browser = await puppeteer.launch({
+  const defaultPuppeteerLaunchOptions = {
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
+  }
+  const browser = await puppeteer.launch(defu(options?.puppeteerLaunchOpions ?? {}, defaultPuppeteerLaunchOptions));
   const page = await browser.newPage();
 
   await page.setContent(html, {
     waitUntil: 'domcontentloaded'
   });
 
-  const defaultOptions: PDFOptions = {
+  const defaultPdfOptions: PDFOptions = {
     format: 'A4',
     printBackground: true
   };
-  const pdfBuffer = await page.pdf(defu(options?.pdfOptions ?? {}, defaultOptions));
+  const pdfBuffer = await page.pdf(defu(options?.pdfOptions ?? {}, defaultPdfOptions));
 
   await browser.close();
 
